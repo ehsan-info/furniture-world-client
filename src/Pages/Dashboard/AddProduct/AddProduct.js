4fb434dfb02590bd12bd84576c16c141e1494e3c
ehsan-info <ehsanurrahman1992@gmail.com>
2022-11-28 13:22:57 +0200

    my product deleted

--- src/Pages/Dashboard/AddProduct/AddProduct.js
+++ src/Pages/Dashboard/AddProduct/AddProduct.js
@@ -1,9 +1,181 @@
-import React from 'react';
-
+import { useQuery } from '@tanstack/react-query';
+import React, { useContext } from 'react';
+import { useForm } from 'react-hook-form';
+import toast from 'react-hot-toast';
+import { json, useNavigate } from 'react-router-dom';
+import Loader from '../../../Shared/Loader/Loader';
+import { AuthContext } from '../../../Contexts/AuthProvider';
 const AddProduct = () => {
+    const { user } = useContext(AuthContext);
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    const imageHostKey = process.env.REACT_APP_imgbb_key;
+    const navigate = useNavigate();
+    // console.log(imageHostKey);
+    const { data: categories, isLoading } = useQuery({
+        queryKey: ['category'],
+        queryFn: async () => {
+            const res = await fetch(`http://localhost:5000/categories`);
+            const data = await res.json();
+            return data;
+        }
+    })
+    const handleAddProduct = data => {
+        // console.log(data);
+        // console.log(data.image[0]);
+        const image = data.image[0];
+        const formData = new FormData();
+        formData.append('image', image);
+        const url = `https://api.imgbb.com/1/upload?key=${imageHostKey}`;
+        fetch(url, {
+            method: 'POST',
+            body: formData
+        })
+            .then(res => res.json())
+            .then(imgData => {
+                console.log(imgData);
+                if (imgData.success) {
+                    const product = {
+                        image: imgData.data.url,
+                        product_name: data.product_name,
+                        product_category: data.category,
+                        resale_price: data.resale_price,
+                        original_price: data.original_price,
+                        purchase_date: data.purchase_date,
+                        condition: data.condition,
+                        description: data.description,
+                        location: data.location,
+                        seller_name: data.seller_name,
+                        seller_email: data.seller_email,
+                        phone_number: data.phone_number,
+                        available: 'available',
+                        advertised: 'advertised',
+                        posted_date: new Date()
+
+                    }
+
+                    // console.log(imgData.data.url);
+                    //save products to the database
+                    fetch(`http://localhost:5000/addproduct`, {
+                        method: 'POST',
+                        headers: {
+                            'content-type': 'application/json',
+                            authorization: `bearer ${localStorage.getItem('accessToken')}`
+                        },
+                        body: JSON.stringify(product)
+                    })
+                        .then(res => res.json())
+                        .then(result => {
+                            console.log(result);
+                            toast.success(`${data.product_name} is added successfully`);
+                            navigate('/dashboard/myproducts')
+                        })
+                }
+            })
+    }
+    if (isLoading) {
+        return <Loader></Loader>
+    }
     return (
-        <div>
-            <h2>this is add product</h2>
+        <div className='w-96 p-7'>
+            <h2 className='text-3xl'>Add a Product</h2>
+            <form onSubmit={handleSubmit(handleAddProduct)}>
+                <div className="form-control w-full max-w-xs">
+                    <label className="label"><span className="label-text">Add Image</span></label>
+                    <input type="file" {...register("image",
+                        {
+                            required: "Image is required"
+                        })}
+                        className="input input-bordered w-full max-w-xs" />
+                    {errors.image && <p className='text-red-500'>{errors.image.message}</p>}
+                </div>
+                <div className="form-control w-full max-w-xs">
+                    <label className="label"><span className="label-text">Product Name</span></label>
+                    <input type="text" {...register("product_name",
+                        {
+                            required: "Product Name is required"
+                        })}
+                        className="input input-bordered w-full max-w-xs" />
+                    {errors.product_name && <p className='text-red-500'>{errors.product_name.message}</p>}
+                </div>
+                <div className="form-control w-full max-w-xs">
+                    <label className="label"><span className="label-text">Category</span></label>
+                    <select {...register("category", { required: "Category is Required" })} className="select select-bordered w-full max-w-xs">
+                        <option disabled >Please Select a Category</option>
+                        {
+                            categories.map(category => <option key={category._id} defaultValue={category.category_title}>{category.category_title}</option>)
+                        }
+                    </select>
+                </div>
+                <div className="form-control w-full max-w-xs">
+                    <label className="label"><span className="label-text">Resale Price</span></label>
+                    <input type="text" {...register("resale_price",
+                        {
+                            required: "Resale Price is required"
+                        })}
+                        className="input input-bordered w-full max-w-xs" />
+                    {errors.resale_price && <p className='text-red-500'>{errors.resale_price.message}</p>}
+                </div>
+                <div className="form-control w-full max-w-xs">
+                    <label className="label"><span className="label-text">Original Price</span></label>
+                    <input type="text" {...register("original_price",
+                        {
+                            required: "Original Price is required"
+                        })}
+                        className="input input-bordered w-full max-w-xs" />
+                    {errors.original_price && <p className='text-red-500'>{errors.original_price.message}</p>}
+                </div>
+                <div className="form-control w-full max-w-xs">
+                    <label className="label"><span className="label-text">Purchase Date</span></label>
+                    <input type="date" {...register("purchase_date",
+                        {
+                            required: "Purchase Date is required"
+                        })}
+                        className="input input-bordered w-full max-w-xs" />
+                    {errors.purchase_date && <p className='text-red-500'>{errors.purchase_date.message}</p>}
+                </div>
+                <div className="form-control w-full max-w-xs">
+                    <label className="label"><span className="label-text">Condition</span></label>
+                    <select {...register("condition", { required: "Condition is Required" })} className="select select-bordered w-full max-w-xs">
+                        <option disabled>Please Select Condition</option>
+                        <option value="excellent">Excellent</option>
+                        <option value="good">Good</option>
+                        <option value="fair">Fair</option>
+                    </select>
+                    {errors.condition && <p className='text-red-500'>{errors.condition.message}</p>}
+                </div>
+                <div className="form-control w-full max-w-xs">
+                    <label className="label"><span className="label-text">Description</span></label>
+                    <input type="text" {...register("description")}
+                        className="input input-bordered w-full max-w-xs" />
+                </div>
+                <div className="form-control w-full max-w-xs">
+                    <label className="label"><span className="label-text">Location</span></label>
+                    <input type="text" {...register("location",
+                        {
+                            required: "Location is required"
+                        })}
+                        className="input input-bordered w-full max-w-xs" />
+                    {errors.location && <p className='text-red-500'>{errors.location.message}</p>}
+                </div>
+                <div className="form-control w-full max-w-xs">
+                    <label className="label"><span className="label-text">Seller Name</span></label>
+                    <input defaultValue={user?.displayName} type="text" {...register("seller-name")} className="input input-bordered w-full max-w-xs" disabled />
+                </div>
+                <div className="form-control w-full max-w-xs">
+                    <label className="label"><span className="label-text">Seller Email</span></label>
+                    <input type="email" defaultValue={user?.email}  {...register("seller_email")} className="input input-bordered w-full max-w-xs" disabled />
+                </div>
+                <div className="form-control w-full max-w-xs">
+                    <label className="label"><span className="label-text">Phone Number</span></label>
+                    <input type="text" {...register("phone_number",
+                        {
+                            required: "Phone Number is required"
+                        })}
+                        className="input input-bordered w-full max-w-xs" />
+                    {errors.phone_number && <p className='text-red-500'>{errors.phone_number.message}</p>}
+                </div>
+                <input className='btn btn-accent w-full mt-8' value='Add Product' type="submit" />
+            </form>
         </div>
     );
 };
--- src/Pages/Dashboard/MyProducts/MyProducts.js
+++ src/Pages/Dashboard/MyProducts/MyProducts.js
@@ -1,9 +1,103 @@
-import React from 'react';
+import { useQuery } from '@tanstack/react-query';
+import React, { useState } from 'react';
+import toast from 'react-hot-toast';
+import Loader from '../../../Shared/Loader/Loader';
+import ConfirmationModal from '../../../Shared/ConfirmationModal/ConfirmationModal';
 
 const MyProducts = () => {
+    const [deletingProduct, setDeletingProduct] = useState(null);
+    const closeModal = () => {
+        setDeletingProduct(null);
+    }
+
+    const { data: products = [], isLoading, refetch } = useQuery({
+        queryKey: ['products'],
+        queryFn: async () => {
+            try {
+                const res = await fetch(`http://localhost:5000/products`, {
+                    headers: {
+                        authorization: `bearer ${localStorage.getItem('accessToken')}`
+                    }
+                })
+                const data = await res.json();
+                return data;
+            }
+            catch (error) {
+
+            }
+        }
+    })
+
+    const handleDeleteProduct = product => {
+        console.log(product);
+        fetch(`http://localhost:5000/products/${product._id}`, {
+            method: 'DELETE',
+            headers: {
+                authorization: `bearer ${localStorage.getItem('accessToken')}`
+            }
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                if (data.deletedCount > 0) {
+                    toast.success(`Product ${product.product_name} Deleted  Successfully`)
+                    refetch();
+                }
+
+            })
+    }
+    if (isLoading) {
+        return <Loader></Loader>
+    }
     return (
         <div>
-            <h2>this is my products</h2>
+            <h2 className='text-3xl mb-4'>My Products: {products?.length}</h2>
+            <div className="overflow-x-auto">
+                <table className="table w-full">
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Avatar</th>
+                            <th>Product Name</th>
+                            <th>Resale Price</th>
+                            <th>Category</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+
+                        {
+                            products.map((product, i) => <tr key={product._id}>
+                                <th>{i + 1}</th>
+                                <td>
+                                    <div className="avatar">
+                                        <div className="w-24 rounded-full">
+                                            <img src={product.image} alt="" />
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>{product.product_name}</td>
+                                <td>{product.resale_price}</td>
+                                <td>{product.product_category}</td>
+                                <td>
+                                    <label onClick={() => setDeletingProduct(product)} htmlFor="confirmation-modal" className="btn btn-sm btn-error">Delete</label>
+                                </td>
+                            </tr>)
+                        }
+                    </tbody>
+                </table>
+            </div>
+            {
+                deletingProduct &&
+                <ConfirmationModal
+                    title={`Are you sure you want to delete?`}
+                    message={`If you delete ${deletingProduct.product_name} it can not be undone`}
+                    closeModal={closeModal}
+                    successButtonName="Delete"
+                    successAction={handleDeleteProduct}
+                    modalData={deletingProduct}>
+                </ConfirmationModal>
+            }
         </div>
     );
 };
