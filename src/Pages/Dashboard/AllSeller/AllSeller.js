e5a1045f5d5031bcc9d7ccbcc854ed8dffa08986
ehsan-info <ehsanurrahman1992@gmail.com>
2022-11-28 09:34:28 +0200

    dashboard pages added

---
+++ src/Pages/Dashboard/AllSeller/AllSeller.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AllSeller = () => {
+    return (
+        <div>
+            <h2>this is all seller</h2>
+        </div>
+    );
+};
+
+export default AllSeller;
